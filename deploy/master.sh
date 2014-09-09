74c89b87c77953abcc887f42dabc1caac3e6ad86
Josh Hunt <josh@trtr.co>
2014-09-09 15:59:52 +1000

    Updated deployment scripts

--- deploy/master.sh
+++ deploy/master.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-echo "Received updates, deploying master to production"
 
-GIT_WORK_TREE=/home/josh/www/shifts-api git checkout -f
-cd /home/josh/www/shifts-api
+echo "Received updates, deploying master"
+
+CODE_DIR="/home/josh/www/shifts-api"
+GIT_WORK_TREE=$CODE_DIR
+
+# Checkout the latest code and move into that dir
+git checkout -f
+cd $CODE_DIR
+
+# Do app-specific build tasks
 npm install
 forever restart api
+
 echo "Deployed changes to server 👍"
