9f905e33e69f13bcf7fc66b149ae718c96c7b1bf
Josh Hunt <josh@trtr.co>
2014-09-09 14:57:51 +1000

    Updated deploy script

--- deploy/master.sh
+++ deploy/master.sh
@@ -3,6 +3,8 @@ read OLDID NEWID BRANCH
 REF_PREFIX="refs/heads/"
 BRANCH=${BRANCH_PATH#${REF_PREFIX}}
 
+echo "Hey cool! This is the checked-in deploy script!"
+
 echo "Received updates, deploying $BRANCH"
 
 GIT_WORK_TREE=/home/josh/www/shifts-api git checkout -f
