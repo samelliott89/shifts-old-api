108033f17d54fe16bf83e5f64ce275aac2d77b45
Josh Hunt <josh@trtr.co>
2014-09-09 17:34:58 +1000

    Updated deployment scripts

--- deploy/master.sh
+++ deploy/master.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 echo $'\e[1G'"------> Recieved update, deploying master"
-echo $'\e[1G\e[K'"Deploying master"
 
 CODE_DIR="/home/josh/www/shifts-api"
 
 # Checkout the latest code and move into that dir
-GIT_WORK_TREE=$CODE_DIR git checkout -f | sed $'s/^/\e[1G\e[K/'
+echo $'\e[1G'"------> Checking out latest master into $CODE_DIR"
+GIT_WORK_TREE=$CODE_DIR git checkout -f | sed $'s/^/\e[1G        /'
 cd $CODE_DIR
 
 # Do app-specific build tasks
-npm install | sed $'s/^/\e[1G\e[K/'
-forever restart api | sed $'s/^/\e[1G\e[K/'
+echo $'\e[1G'"------> Installing NPM dependencies"
+npm install | sed $'s/^/\e[1G        /'
+
+echo $'\e[1G'"------> Finally, restarting server"
+forever restart api | sed $'s/^/\e[1G        /'
 
 echo $'\e[1G'"------> Deployed changes to server 👍"
